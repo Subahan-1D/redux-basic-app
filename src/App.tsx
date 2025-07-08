6f390fe8d391d151f91bd36a1ae9d22e284281fc
subahan ali <subahanislam523@gmail.com>
2025-07-08 11:57:44 +0600

    added to by a shadcn ui or redux typescript project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>basic redux app</h1>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
